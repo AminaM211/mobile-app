2be686ff99615b86b7596336b2f63b5dfa250cfa
AminaM211 <amina_moutalibov@hotmail.com>
2025-02-27 23:45:01 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./screens/HomeScreen.js";
+import ProductDetails from "./screens/ProductDetails.js";
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+const Stack = createStackNavigator();
+
+ 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator> 
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="ProductDetails" component={ProductDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
